d455340c8006e39c6098b72d864f44aec7551a03
Joe Plumb <hello@joeplumb.com>
2018-08-17 14:43:42 +0100

    remove default tests

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
-FROM openjdk
-ADD target/catalog-0.0.1-SNAPSHOT.jar app.jar
-ENTRYPOINT ["java", "-jar", ""]
+FROM openjdk:8-jre-slim
+EXPOSE 8080
+ADD ./target/catalog-search-0.0.1-SNAPSHOT.jar app.jar
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/emart/catalogsearch/CatalogSearchApplication.java
+++ src/main/java/com/emart/catalogsearch/CatalogSearchApplication.java
@@ -8,8 +8,8 @@ public class CatalogSearchApplication {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("javax.net.ssl.trustStore","c:/Users/JOSEPHPlumb/java/jre.1.8.0_181/lib/security/cacerts");
-        System.setProperty("javax.net.ssl.trustStorePassword","changeit");
+		//System.setProperty("javax.net.ssl.trustStore","c:/Users/JOSEPHPlumb/java/jre.1.8.0_181/lib/security/cacerts");
+        //System.setProperty("javax.net.ssl.trustStorePassword","changeit");
 		
 		SpringApplication.run(CatalogSearchApplication.class, args);
 	}
